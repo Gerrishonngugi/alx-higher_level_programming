06c9ce075c6e5984fe01f5b642e78efb121a963d
Gerrishonngugi <gerrishonngugi@gmail.com>
2024-02-12 23:54:01 -0800

    Python Test Driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines the text_indentation module
+
+"""
+
+
+def text_indentation(text):
+    """
+     prints text with 2 lines after each characters
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if (text[i] in ".?:"):
+            print(text[i], end="\n\n")
+        else:
+            if (text[i - 1] in ".?: ") and i != 0 and text[i] == ' ':
+                continue
+            print(text[i], end="")
