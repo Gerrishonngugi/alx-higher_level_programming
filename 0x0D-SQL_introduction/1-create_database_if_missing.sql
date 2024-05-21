d9966efe27854fdf93b6c8c7f19caaf23bd024ec
Gerrishonngugi <gerrishonngugi@gmail.com>
2024-05-21 02:49:05 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- second script 
+-- script to create db if doesnt exist already
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script 10
+-- script that lists records of table second_table of the database
+-- hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table ORDER BY score DESC;
